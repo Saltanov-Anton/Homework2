1b54edb070106543373f0fdee54bc74abababfdb
Anton Saltanov <saltanov.anton@yandex.ru>
2022-11-29 16:01:38 +0900

    Увеличил значение переменных.

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,12 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+        dog += 4;
+        cat += 4;
+        paper += 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
